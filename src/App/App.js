550f0f7860f13d85720dd294a4216bc19e6404a2
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-02-19 17:49:39 -0600

    setup react app

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <h1> Users</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
